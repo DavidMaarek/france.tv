657115cc4363f0f9918ba9b0fa6c15994131504b
David Maarek <davidmaarek55@gmail.com>
2017-06-15 11:50:55 +0200

    Finalisation init

--- gulpfile.js
+++ gulpfile.js
@@ -7,13 +7,13 @@ var gulp = require("gulp");
 var sass = require("gulp-sass");
 
 gulp.task('sass', function () {
-    return gulp.src('web/sass/*.scss')
+    return gulp.src('scss/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('web/css'));
+        .pipe(gulp.dest('css'));
 });
 
 gulp.task('watch', function () {
-    gulp.watch('web/sass/*.scss', ['sass']).on('change', function(event){
+    gulp.watch('scss/*.scss', ['sass']).on('change', function(event){
         console.log('Le fichier ' + event.path + ' a ete compile');
     })
 });
