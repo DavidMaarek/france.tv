3c4e43743cbd2cf3505e6adbb79b77800a885a58
David Maarek <davidmaarek55@gmail.com>
2017-06-22 09:59:43 +0200

    Dashboard terminé et page des choix aussi mais seulement pour le fonctionnel pas pour le contenu

--- gulpfile.js
+++ gulpfile.js
@@ -4,15 +4,19 @@
 
 var gulp = require("gulp");
 
-var sass = require("gulp-sass");
+var sass = require("gulp-sass"),
+    livereload = require('gulp-livereload');
+
 
 gulp.task('sass', function () {
     return gulp.src('scss/*.scss')
         .pipe(sass().on('error', sass.logError))
-        .pipe(gulp.dest('css'));
+        .pipe(gulp.dest('css'))
+        .pipe(livereload());
 });
 
 gulp.task('watch', function () {
+    livereload.listen();
     gulp.watch('scss/*.scss', ['sass']).on('change', function(event){
         console.log('Le fichier ' + event.path + ' a ete compile');
     })
